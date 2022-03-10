2574a60503283cfaa1263e29f075252e7342e6a6
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-03-10 23:02:52 +0530

    Create linux.cmd

---
+++ linux.cmd
@@ -0,0 +1,36 @@
+elcot@boss:~$ pwd
+/home/elcot
+elcot@boss:~$ cd
+elcot@boss:~$ ls
+ArithmeticOperator.py  Downloads  Music     task1      valla
+Desktop                frthu      Pictures  task2      Videos
+Documents              hrllo.txt  Public    Templates
+elcot@boss:~$ mkdir sample
+elcot@boss:~$ rmdir sample/
+elcot@boss:~$ gedit
+elcot@boss:~$ rmdir Documents/sample
+rmdir: failed to remove 'Documents/sample': No such file or directory
+elcot@boss:~$ rm-r
+bash: rm-r: command not found
+elcot@boss:~$ cd
+elcot@boss:~$ touch good.txt
+elcot@boss:~$ mv good.txt hello.txt
+elcot@boss:~$ cat hello.txt
+elcot@boss:~$ echo(display in file)
+bash: syntax error near unexpected token `display'
+elcot@boss:~$ echo
+
+elcot@boss:~$ cat > hello.txt
+welcome
+good
+^C
+elcot@boss:~$ cat >> hello.txt
+
+linux its easy to learn
+^C
+elcot@boss:~$ grep"good"
+bash: grepgood: command not found
+elcot@boss:~$ ^C
+elcot@boss:~$
+
+
