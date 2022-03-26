0628714caec37ee69e47c9073c64e6aa4467f7a2
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-03-26 18:45:32 +0530

    Create CSV_example.py

---
+++ CSV_example.py
@@ -0,0 +1,12 @@
+
+f=open('simple.txt')
+print(f.read())
+
+f=open('simple.txt','w')
+print(f.write("beast,villu,ghilli,mersal,kathi,thuppaki,nanban,bgil,master,theri,sarkar,pokkiri,puli,bairavaa,thaliva,velayudham,kuruvi,sura,sachein,thirupachi,vasegara,frinds,kushi,sivakasi,aathi,bagavathi,badri,youth,kuruvi,"))
+
+f=open('simple.txt','a')
+f=f.write('welcome')
+
+
+
