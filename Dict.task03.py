e39dcb787c01adcb789444e5bba8124a1e9f2fab
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-04-07 18:57:11 +0530

    Create Dict.task03.py

---
+++ Dict.task03.py
@@ -0,0 +1,17 @@
+d={1:{"name":"frathunan","dept":"cse","year":4},
+  2:{"name":"gopi","dept":"b.com","year":3},
+  3:{"name":"santhosh","dept":"EEE","year":4}}
+b=list(d.keys())
+x = int(input("Enter the stud_id:"))
+if x in b:
+    for key, value in d[x].items():
+        print(key, ":", value)
+
+
+
+
+#Result
+Enter the stud_id:1
+name : frathunan
+dept : cse
+year : 4
