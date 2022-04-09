97d106d7959569999829b78aa87340c2641ecbe4
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-04-09 22:58:21 +0530

    Create task08.py

---
+++ task08.py
@@ -0,0 +1,14 @@
+class Employee:
+    x = 10
+    def func_msg(self):
+        print('Welcome to Employee Class')
+class Department(Employee):
+    a = 250
+    b = Employee.x + 22
+    def func_message(self):
+        print('Welcome to Department Class')
+    def func_changed(self):
+        print('New Value = ', Employee.x + 449)
+emp = Employee()
+print(emp.x)
+emp.func_msg()
