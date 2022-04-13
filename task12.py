df011ca8285fbcf62ac7e48c4744a4a06737fa9e
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-04-13 20:32:51 +0530

    Create task12.py

---
+++ task12.py
@@ -0,0 +1,31 @@
+a=int(input("Enter the value :"))
+sum=0
+for i in range(1,a+1):
+    m=int(input("Enter the number:"))
+    sum=sum+m
+    print(sum)
+print(sum)
+
+#Result
+Enter the value :10
+Enter the number:10
+10
+Enter the number:20
+30
+Enter the number:30
+60
+Enter the number:10
+70
+Enter the number:20
+90
+Enter the number:30
+120
+Enter the number:30
+150
+Enter the number:40
+190
+Enter the number:50
+240
+Enter the number:20
+260
+260
