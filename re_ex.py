5155e2f5dcd5b47b1502d9a5eecde585041c2c02
vallarasu <100554866+vallaras@users.noreply.github.com>
2022-07-15 20:04:36 +0530

    Add files via upload

---
+++ re_ex.py
@@ -0,0 +1,289 @@
+import re
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.search("\s", txt)
+
+print("The first white-space character is located in position:", x.start())
+
+txt = "The rain in Spain"
+x = re.search("Portugal", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt, 1)
+print(x)
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+print(x.string)
+print(x.group())
+
+txt = "The rain in Spain"
+
+#Find all lower case characters alphabetically between "a" and "m":
+
+x = re.findall("[a-m]", txt)
+print(x)
+
+txt = "That will be 59 dollars"
+
+#Find all digit characters:
+
+x = re.findall("\d", txt)
+print(x)
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by two (any) characters, and an "o":
+
+x = re.findall("he..o", txt)
+print(x)
+
+txt = "vvvv hello planet"
+
+#Check if the string starts with 'hello':
+
+x = re.findall("^hello", txt)
+if x:
+  print("Yes, the string starts with 'hello'")
+else:
+  print("No match")
+
+txt = "hello planet2222"
+
+#Check if the string ends with 'planet':
+
+x = re.findall("planet$", txt)
+if x:
+  print("Yes, the string ends with 'planet'")
+else:
+  print("No match")
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 0 or more  (any) characters, and an "o":
+
+x = re.findall("he.*o", txt)
+
+print(x)
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed excactly 2 (any) characters, and an "o":
+
+x = re.findall("he.{2}o", txt)
+
+print(x)
+
+txt = "The rain in Spain falls mainly in the plain!"
+
+#Check if the string contains either "falls" or "stays":
+
+x = re.findall("falls|stays", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+txt = "The rain in Spain"
+
+#Check if the string starts with "The":
+
+x = re.findall("\AThe", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is a match!")
+else:
+  print("No match")
+
+
+txt = "The rain in Spain"
+
+#Check if "ain" is present at the beginning of a WORD:
+
+x = re.findall(r"\bain", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+
+
+
+txt = "The rain in Spain"
+
+#Check if the string has any a, r, or n characters:
+
+x = re.findall("[arn]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+
+
+txt = "The rain in Spain"
+
+#Check if the string has any characters between a and n:
+
+x = re.findall("[a-n]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+
+
+txt = "The rain in Spain"
+
+#Check if the string has other characters than a, r, or n:
+
+x = re.findall("[^arn]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+
+txt = "The rain in Spain"
+
+#Check if the string has any 0, 1, 2, or 3 digits:
+
+x = re.findall("[0123]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any digits:
+
+x = re.findall("[0-9]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+
+
+txt = "8 times before 11:75 AM"
+
+#Check if the string has any two-digit numbers, from 00 to 59:
+
+x = re.findall("[0-5][0-9]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+txt = "mobile number 6374279569"
+
+#Check if the string has any two-digit numbers, from 00 to 59:
+
+x = re.findall("[6-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+txt=input("enter the string")
+x = re.findall("[a-z]|\s", txt)
+y = re.findall("\d", txt)
+print(len(x))
+print(len(y))
+
+txt="i love india 23783"
+for a in txt:
+  print(a,ord(a))
+x=re.findall("\s",txt)
+y=re.findall("\d", txt)
+z=re.findall("[a-z]",txt)
+print(len(x))
+print(len(y))
+print(len(z))
+
+x=97
+y=32
+z=49
+scount=0
+lcount=0
+ncount=0
+
+txt="i love india 1234"
+for a in txt:
+  if ord(a)>=97and ord(a)<=123:
+    lcount+=1
+  elif ord(a)>=49and ord(a)<=58:
+    ncount+=1
+  else:
+    scount+=1
+print(lcount,ncount,scount)
+
+
+
+
+
